f47104d5591094886f881e912cafa24d6f9a6f33
srikanthsamaraweera <samaraweeraster@gmail.com>
2024-12-20 11:57:08 -0500

    production build related changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+        SiteURL: process.env.SiteURL,
+        AUTH_SECRET: process.env.AUTH_SECRET,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    },
+};
 
 export default nextConfig;
